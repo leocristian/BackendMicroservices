ac85b12f19ade5c42d65cf60bd910c3566534fd5
leocristian <leonardosclopes@gmail.com>
2024-01-07 19:45:16 -0300

    add autorizacao enfermeiro cadastrar paciente

--- AppBackend/ApiGateway/Controllers/ConsultasService/PacientesController.cs
+++ AppBackend/ApiGateway/Controllers/ConsultasService/PacientesController.cs
@@ -58,7 +58,7 @@ namespace ApiGateway.Controllers {
         }
 
         [HttpPost]
-        [Authorize(Policy = "Usuarios")]
+        [Authorize(Policy = "Enfermeiros")]
         [Route("api/consultas/pacientes/novo")]
         public async Task<IActionResult> Insert([FromBody] Paciente paciente) {
 
