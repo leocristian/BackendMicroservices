3cd6cee155883f92fb763650b532df92f19532bf
leocristian <leonardosclopes@gmail.com>
2024-02-18 09:22:35 -0300

    add proxy reverso

--- AppBackend/EnfermeiroService/Program.cs
+++ AppBackend/EnfermeiroService/Program.cs
@@ -1,19 +1,27 @@
 using EnfermeiroService.Connection;
 using EnfermeiroService.Services;
 using Npgsql;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-builder.Services.AddCors(options => {
-    options.AddPolicy(
-        name: MyAllowSpecificOrigins,
-        policy => {
-            policy.WithOrigins("http://ec2-54-90-88-53.compute-1.amazonaws.com");
-        }
-    );
+// Configure forwarded headers
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.KnownProxies.Add(IPAddress.Parse("3.87.199.70"));
 });
+
+// var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// builder.Services.AddCors(options => {
+//     options.AddPolicy(
+//         name: MyAllowSpecificOrigins,
+//         policy => {
+//             policy.WithOrigins("http://ec2-54-90-88-53.compute-1.amazonaws.com");
+//         }
+//     );
+// });
 
 // Add services to the container.
 
@@ -31,15 +39,23 @@ builder.Services.AddScoped<UsuarioService>();
 
 var app = builder.Build();
 
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseCors(MyAllowSpecificOrigins);
+// app.UseCors(MyAllowSpecificOrigins);
+
+app.MapGet("/", () => "Deu certo meu ForwardedHeaderOptions!");
 
 app.UseAuthorization();
 
